83ccb85121a6d48006aa0b849d49c9315668397b
erikanjb1 <enjimene@cougarnet.uh.edu>
2026-03-26 20:01:41 -0500

    Update delete_animal.php

--- webapp/delete_animal.php
+++ webapp/delete_animal.php
@@ -1,11 +1,16 @@
 <?php
-require_once __DIR__ . '/../db.php';
-
-$id = $_GET["id"];
-
-$stmt = $pdo->prepare("DELETE FROM animals WHERE AnimalID = ?");
-$stmt->execute([(int)$id]);
-
-header("Location: animals_report.php");
-exit();
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.html');
+    exit;
+}
+require 'db.php';
+ 
+$id = $_GET['id'];
+$stmt = $pdo->prepare("DELETE FROM animal WHERE Animal_ID = ?");
+$stmt->execute([$id]);
+ 
+header('Location: animals_report.php');
+exit;
 ?>
+ 
