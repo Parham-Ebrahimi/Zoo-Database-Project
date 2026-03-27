c5c8b5b3d1592dc6edb3f84b522c6ce1ed22f529
Parham-Ebrahimi <parham.ebrahimi123@gmail.com>
2026-03-26 19:22:54 -0500

    Fix edit/delete pages, add forms, triggers, and scrolling fixes

--- webapp/delete_animal.php
+++ webapp/delete_animal.php
@@ -1,9 +1,9 @@
 <?php
-require_once __DIR__ . '/../db.php';
+require_once 'db.php';
 
 $id = $_GET["id"];
 
-$stmt = $pdo->prepare("DELETE FROM animals WHERE AnimalID = ?");
+$stmt = $pdo->prepare("DELETE FROM animal WHERE Animal_ID = ?");
 $stmt->execute([(int)$id]);
 
 header("Location: animals_report.php");
